63e4da8aad08618f64f5fc09d4d05881743ed0b4
k-tsunoda615 <kntrtnd@gmail.com>
2025-04-17 22:12:44 +0900

    todo削除ボタンの復活

--- components/TodoCard.vue
+++ components/TodoCard.vue
@@ -9,8 +9,8 @@
           v-html="parsedMemo"
         />
       </div>
-      <div class="flex items-center space-x-2">
-        <UBadge v-if="todo.is_private" color="gray" size="sm">
+      <div class="flex items-center">
+        <UBadge v-if="todo.is_private" color="gray" size="sm" class="mr-2">
           個人タスク
         </UBadge>
         <UButton
@@ -19,6 +19,13 @@
           icon="i-heroicons-pencil-square"
           size="xs"
           @click="$emit('edit', todo)"
+        />
+        <UButton
+          color="red"
+          variant="ghost"
+          icon="i-heroicons-trash"
+          size="xs"
+          @click="deleteTodo"
         />
       </div>
     </div>
@@ -39,6 +46,7 @@ const props = defineProps({
 const emit = defineEmits(["edit"]);
 
 const todoStore = useTodoStore();
+const toast = useToast();
 
 const editTodo = () => {
   emit("edit", props.todo);
@@ -48,8 +56,18 @@ const deleteTodo = async () => {
   if (confirm("このタスクを削除しますか？")) {
     try {
       await todoStore.deleteTodo(props.todo.id);
+      toast.add({
+        title: "削除完了",
+        description: "タスクを削除しました",
+        color: "green",
+      });
     } catch (error) {
       console.error("削除エラー:", error);
+      toast.add({
+        title: "エラー",
+        description: "タスクの削除に失敗しました",
+        color: "red",
+      });
     }
   }
 };
