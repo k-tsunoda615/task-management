2935ef21cf8e4d268d128a373186eb56d64704a6
k-tsunoda615 <kntrtnd@gmail.com>
2025-05-03 20:48:00 +0900

    Header.vue・Footer.vueの共通化

---
+++ components/common/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer
+    class="w-full bg-gray-50 border-t px-4 py-2 text-center text-xs text-gray-500"
+  >
+    &copy; {{ new Date().getFullYear() }} Task Board
+  </footer>
+</template>
---
+++ components/common/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <header
+    class="w-full bg-white border-b px-4 py-2 flex items-center justify-between"
+  >
+    <div class="font-bold text-lg">Task Board</div>
+    <slot />
+  </header>
+</template>
