c15b6ddaec73fa362d01372739bab1ad6b668349
k-tsunoda615 <kntrtnd@gmail.com>
2025-05-31 21:34:30 +0900

    計測時間とタスク名をタイトルに出す

--- composables/useTaskTimer.ts
+++ composables/useTaskTimer.ts
@@ -1,12 +1,28 @@
 import type { Todo } from "../types/todo";
+import { formatTime } from "../utils/time";
 
 export function useTaskTimer() {
   const timerInterval = ref<number | null>(null);
   const startTime = ref<number | null>(null);
   const currentTotalTime = ref(0);
+  const originalTitle = ref(document.title);
+
+  // タブのタイトルを更新する
+  function updateTitle(seconds: number, taskTitle: string) {
+    const formattedTime = formatTime(seconds);
+    document.title = `${formattedTime} - ${taskTitle}`;
+  }
+
+  // タイトルを元に戻す
+  function resetTitle() {
+    document.title = originalTitle.value;
+  }
 
   // タイマーを開始する
   function startTimerForTodo(todo: Todo, onUpdate: (total: number) => void) {
+    // 元のタイトルを保存
+    originalTitle.value = document.title;
+
     currentTotalTime.value = extractTotalTime(todo.total_time);
     startTime.value = Date.now();
     if (timerInterval.value) {
@@ -20,10 +36,15 @@ export function useTaskTimer() {
       if (currentTotalTime.value !== newTotalTime) {
         currentTotalTime.value = newTotalTime;
         onUpdate(currentTotalTime.value);
+        // タブのタイトルを更新
+        updateTitle(currentTotalTime.value, todo.title);
       }
       timerInterval.value = requestAnimationFrame(updateTimer);
     };
     timerInterval.value = requestAnimationFrame(updateTimer);
+
+    // 初回のタイトル更新
+    updateTitle(currentTotalTime.value, todo.title);
   }
 
   // タイマーを停止する
@@ -38,6 +59,9 @@ export function useTaskTimer() {
         extractTotalTime(todo.total_time) + elapsedSeconds;
       startTime.value = null;
     }
+
+    // タイトルを元に戻す
+    resetTitle();
   }
 
   // total_timeから数値を抽出
@@ -55,5 +79,7 @@ export function useTaskTimer() {
     startTimerForTodo,
     stopTimer,
     extractTotalTime,
+    updateTitle,
+    resetTitle,
   };
 }
