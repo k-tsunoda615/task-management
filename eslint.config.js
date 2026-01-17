87674a5b6c4125339ab56da4ecf1eaafec910c60
k-tsunoda615 <kntrtnd@gmail.com>
2026-01-17 14:34:27 +0900

    @nuxt/eslintをインストールしconfigに設定

--- eslint.config.js
+++ eslint.config.js
@@ -1,51 +1,12 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import vue from "eslint-plugin-vue";
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+export default withNuxt({
+  ignores: [
+    "dist",
+    "node_modules",
+    ".nuxt",
+    ".output",
+    "playwright-report",
+    "test-results",
+  ],
 });
-
-export default [
-  {
-    ignores: [
-      "dist",
-      "node_modules",
-      ".nuxt",
-      ".output",
-      "playwright-report",
-      "test-results",
-    ],
-  },
-  ...vue.configs["flat/recommended"],
-  ...compat.config({
-    root: true,
-    env: {
-      browser: true,
-      node: true,
-      es2022: true,
-    },
-    parser: "vue-eslint-parser",
-    parserOptions: {
-      ecmaVersion: 2022,
-      sourceType: "module",
-      parser: "@typescript-eslint/parser",
-    },
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:prettier/recommended",
-    ],
-    plugins: ["@typescript-eslint", "prettier"],
-    rules: {
-      "prettier/prettier": "error",
-    },
-  }),
-];
